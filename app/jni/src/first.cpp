8a695f56f908ca7098b0f9766c1eb3a14dcc336f
Eduardo Pagotto <eduardo.pagotto@newspace.com.br>
2020-05-24 16:48:43 -0300

    running x86

---
+++ app/jni/src/first.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../SDL2/include/SDL.h"
+
+int main(int argc, char *argv[]) {
+
+    SDL_Window* window = nullptr;
+    SDL_GLContext gl = nullptr;
+
+    // Initialize SDL 
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "\nUnable to initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    } 
+
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+
+    SDL_Log("SDL2 Iniciado");
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_DisplayMode mode;
+    SDL_GetDisplayMode(0, 0, &mode);
+    int width = mode.w;
+    int height = mode.h;
+
+    SDL_Log("Width: %d; Height: %d", width, height);
+
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    window = SDL_CreateWindow(nullptr, 0, 0, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    if (window == nullptr) {
+        SDL_Log("Faild to create window %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // OpenGL Context
+    gl = SDL_GL_CreateContext(window);
+    if (gl == nullptr) {
+        SDL_Log("Faild to create Context OpenGL %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main Loop 
+    Uint8 done = 0;
+    SDL_Event event;
+    int count = 0;
+    while(!done) {
+
+        // Check Eventes
+        while(SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN || event.type == SDL_FINGERDOWN)
+            {
+                done = 1;
+            }
+        }
+        SDL_Log("Count: %d", count++);
+
+        SDL_GL_SwapWindow(window);
+        SDL_Delay(100);
+
+    }
+
+    exit(0);
+}
