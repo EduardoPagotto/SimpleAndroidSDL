27fc392d6b6fe9e3ed6af4fe9ebe6ffc3ef90dd5
Eduardo Pagotto <eduardo.pagotto@newspace.com.br>
2020-05-27 22:29:59 -0300

    reorganizado

--- tools-util/link_SDL2_into_project.sh
+++ tools-util/link_SDL2_into_project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ln -s ~/androidlib/SDL2 ./app/jni/SDL2
-ln -s ~/androidlib/SDL2_image ./app/jni/SDL2_image
-ln -s ~/androidlib/SDL2_net ./app/jni/SDL2_net
-ln -s ~/androidlib/SDL2_ttf ./app/jni/SDL2_ttf
-ln -s ~/androidlib/SDL2_mixer ./app/jni/SDL2_mixer
+ln -s ~/androidlib/SDL2 ./android/app/jni/SDL2
+ln -s ~/androidlib/SDL2_image ./android/app/jni/SDL2_image
+ln -s ~/androidlib/SDL2_net ./android/app/jni/SDL2_net
+ln -s ~/androidlib/SDL2_ttf ./android/app/jni/SDL2_ttf
+ln -s ~/androidlib/SDL2_mixer ./android/app/jni/SDL2_mixer
 
